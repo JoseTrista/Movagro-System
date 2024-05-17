fbfc7bf8fd7404b1246e1da886bcc7d12c21f0b0
Oscarpro11 <oscarito.tarazon@gmail.com>
2024-05-16 23:50:14 -0700

    Script Base de datos

---
+++ BD_Movagro.sql
@@ -0,0 +1,24 @@
+INSERT INTO `movagro`.`proveedor` (`ID`, `Direccion`, `Nombre`, `RFC`, `Telefono`) VALUES ('1', 'Norman E Borlaug Sur 9048, Col. Ejido General Ricador Topete', 'TUVANOSA', 'TVN7407019H3', '6444176088');
+INSERT INTO `movagro`.`proveedor` (`ID`, `Direccion`, `Nombre`, `RFC`, `Telefono`) VALUES ('2', 'Blvd. Toluca 13 San Francisco Cuautlalpan Estado de Mexico', 'SOMMER', 'CSO680801P93', '5526298080');
+INSERT INTO `movagro`.`proveedor` (`ID`, `Direccion`, `Nombre`, `RFC`, `Telefono`) VALUES ('3', 'Jacinto Lopez 370. Faustino Felix Serna Obregon', 'Jorge Esteban Legarda Leyva', 'LALJ611204M10', '6444170908');
+INSERT INTO `movagro`.`proveedor` (`ID`, `Direccion`, `Nombre`, `RFC`, `Telefono`) VALUES ('4', 'Vicente Suarez 328 Col. Reforma', 'Aceros y Equipos de Obregon', 'GUIC950805TZ5', '6444135588');
+INSERT INTO `movagro`.`proveedor` (`ID`, `Direccion`, `Nombre`, `RFC`, `Telefono`) VALUES ('5', 'Ignacio Pesqueira Poniente 221', 'PLOMOSA', 'PM0760223442', '6441168600');
+INSERT INTO `movagro`.`proveedor` (`ID`, `Direccion`, `Nombre`, `RFC`, `Telefono`) VALUES ('6', 'Doc Norma E Borlaug Sur 2605 Col. Municipio Libre', 'HSO', 'HSO871003V69', '6444171195');
+INSERT INTO `movagro`.`proveedor` (`ID`, `Direccion`, `Nombre`, `RFC`, `Telefono`) VALUES ('7', 'Calle Hidalgo 513 Interior A1 Col. Centro', 'La casa del carton', 'VAGM961028559', '6444146644');
+
+INSERT INTO Secretaria VALUES("1","Nancy345","nancyAyala@gmail.com","Nancy Ayala","644589632","NancyA");
+
+INSERT INTO MATERIAL VALUES ('1', '0','Tuberias Acero Inoxidable', 'Tubos de acero inox 1/2 pulgada', 'pulgada', '1');
+INSERT INTO MATERIAL VALUES ('2', '0','Tuberias de Acero al Carbono', 'Tubo de acero al carbono 6 pulgadas', 'pulgada', '1');
+INSERT INTO MATERIAL VALUES ('3', '0','Tuberias de Acero Inoxidable Ornamental', 'Tubo inox ornamental 2 pulgadas', 'pulgada', '1');
+INSERT INTO MATERIAL VALUES ('4', '0','PTR', 'Perfiles Tubulares Rectangulares de Acero Inoxidable', 'pulgada', '2');
+INSERT INTO MATERIAL VALUES ('5', '0','Soldadura', 'Capuchon antorcha TIG', 'pieza', '3');
+INSERT INTO MATERIAL VALUES ('6', '0','Soldadura', 'Disco de Corte Acero Inoxidable', 'pieza', '3');
+INSERT INTO MATERIAL VALUES ('7', '0','Planchas de Acero', 'Tubo Redondo Inoxidable', 'pulgadas', '4');
+INSERT INTO MATERIAL VALUES ('8', '0','Abrazaderas', 'Cople rosca interior 1/2 pulgada', 'pulgadas', '5');
+INSERT INTO MATERIAL VALUES ('9', '0','Tuberias PVC', 'Tramo PVC 3/4 pulgadas', 'pulgadas', '5');
+INSERT INTO MATERIAL VALUES ('10', '0','Brocas', 'Disco Flexible LAM', 'piezas', '6');
+INSERT INTO MATERIAL VALUES ('11', '0','Tornillos', 'Rodana plana inoxidable 1 pulgada', 'pulgadas', '6');
+INSERT INTO MATERIAL VALUES ('12', '0','Cajas de Carton', 'Caja EMP trasl 7X7', 'piezas', '7');
+INSERT INTO MATERIAL VALUES ('13', '0','Rollos', 'Rollo de burbuja chica 1.20', 'centimetros', '7');
+
